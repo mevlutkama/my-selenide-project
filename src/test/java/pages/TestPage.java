67401c8c45b35a33665a380ac1f6958e22239968
Mevlut Kama <mtkama02@gmail.com>
2023-04-10 10:54:33 +0300

    selenide

--- src/test/java/pages/TestPage.java
+++ src/test/java/pages/TestPage.java
@@ -5,7 +5,6 @@ import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
-
 public class TestPage {
     // This is my TestCenter page class
     // All objects TestCenter page will go here
@@ -21,4 +20,18 @@ public class TestPage {
     public SelenideElement password = $(By.id("exampleInputPassword1"));
     public SelenideElement submitButton = $(By.xpath("//button[@type='submit']"));
     public SelenideElement logOutLink = $(By.partialLinkText("Logout"));
+
+    // Locating Checkboxes
+    public SelenideElement checkbox1 = $(By.id("box1"));
+    public SelenideElement checkbox2 = $(By.id("box2"));
+
+    // Locating Radios
+    public SelenideElement red =  $(By.id("red"));
+    public SelenideElement yellow = $(By.id("yellow"));
+    public SelenideElement football = $(By.id("football"));
+
+    // Locating Dropdown Elements
+    public SelenideElement year = $(By.id("year"));
+    public SelenideElement month = $(By.id("month"));
+    public SelenideElement day = $(By.id("day"));
 }
