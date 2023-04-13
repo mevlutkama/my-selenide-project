4426188882d7d1f625468b1386ba4f840c29b911
Mevlut Kama <mtkama02@gmail.com>
2023-04-13 10:27:25 +0300

    selenide

--- src/test/java/pages/GooglePage.java
+++ src/test/java/pages/GooglePage.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.By;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 public class GooglePage {
-
     public SelenideElement googleSearchBox = $(By.name("q"));
     public SelenideElement resultSection = $(By.id("result-stats"));
     public ElementsCollection getAllSections = $$(By.xpath("//*[@class='g']"));
