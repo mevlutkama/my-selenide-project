016bd012d30bb5bf2fc617b9d612680b21679bb8
Mevlut Kama <mtkama02@gmail.com>
2023-04-13 10:26:55 +0300

    selenide

--- src/test/java/pages/GooglePage.java
+++ src/test/java/pages/GooglePage.java
@@ -5,7 +5,6 @@ import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
-
 public class GooglePage {
 
     public SelenideElement googleSearchBox = $(By.name("q"));
