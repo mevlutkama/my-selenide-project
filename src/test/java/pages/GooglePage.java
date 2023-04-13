78a209d258865f4feb78f413982f10666335e929
Mevlut Kama <mtkama02@gmail.com>
2023-04-13 10:26:20 +0300

    selenide

--- src/test/java/pages/GooglePage.java
+++ src/test/java/pages/GooglePage.java
@@ -3,7 +3,6 @@ package pages;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
-
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
