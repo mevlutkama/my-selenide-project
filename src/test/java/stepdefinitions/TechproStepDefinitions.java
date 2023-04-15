9a1e06348e1dfac2579186ed622fe91c6c0be5a6
Mevlut Kama <mtkama02@gmail.com>
2023-04-15 15:18:56 +0300

    selenide

--- src/test/java/stepdefinitions/TechproStepDefinitions.java
+++ src/test/java/stepdefinitions/TechproStepDefinitions.java
@@ -7,7 +7,6 @@ import pages.TestPage;
 import static com.codeborne.selenide.Selenide.*;
 public class TechproStepDefinitions {
     TestPage testPage = new TestPage();
-
     @Given("I get the title of the current page and verify if it contains {string}")
     public void i_get_the_title_of_the_current_page_and_verify_if_it_contains(String string) {
         // getting the title of the page
