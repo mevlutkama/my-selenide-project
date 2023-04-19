5b4f94ba6930bf694f330667fec29814f2dd799f
Mevlut Kama <mtkama02@gmail.com>
2023-04-19 11:09:20 +0300

    selenide

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -12,6 +12,7 @@ import org.junit.runner.RunWith;
                 "junit:target/xml-report/cucumber.xml",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
         },
+
         monochrome=true,
         features = "./src/test/resources/features",
         glue = {"stepdefinitions"},
